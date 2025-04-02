141ea3b0574f378c89680607881dd7d42c290209
Hugo <ducky4tools@gmail.com>
2025-04-02 13:50:39 +0200

    Création hook call api + création composant qui affiche le nom & prenom

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import PlanningPage from './pages/PlanningPage/PlanningPage.tsx';
 import NotFoundPage from './pages/NotFoundPage/NotFoundPage.tsx';
+import DashboardPage from './pages/DashboardPage/DashboardPage.tsx';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<PlanningPage />} />
+        <Route path="/" element={<DashboardPage />} />
         <Route path="*" element={<NotFoundPage/>}/>
       </Routes>
     </Router>
--- src/pages/PlanningPage/PlanningPage.tsx
+++ src/pages/PlanningPage/PlanningPage.tsx
@@ -1,9 +1,11 @@
 import "../../app/styles/global.scss";
+import DisplayProfilName from "../../components/sidebar/profilName/displayProfilName";
 
 const PlanningPage: React.FC = () => {
     return (
         <div>
             <h1>Planning page</h1>
+            <DisplayProfilName firstname="Hugo" name="Pigeon"/>
         </div>
     );
   };
