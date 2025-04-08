f91cd6ae2f2ecc6b039b718f8dfb744686b0bbe8
Elise-44 <bouardelise.perso@gmail.com>
2025-04-08 22:03:01 +0200

    correction

--- src/components/layout/IconButton/IconButton.tsx
+++ src/components/layout/IconButton/IconButton.tsx
@@ -1,54 +1,55 @@
 import React from "react";
-import IconButtonStyle from "./IconButton.module.scss"
+import styles from "./IconButton.module.scss";
+import clsx from "clsx";
 
-// Interface pour les props du bouton
 interface IconButtonProps {
-    text: string; // Le texte du bouton
-    color?: string; // Couleur du texte
-    fontWeight?: "regular" | "medium" | "semibold" | "bold"; // Poids de la police
-    isActive?: boolean;
-    isDisabled?: boolean;
-    onClick?: () => void;
-    specialClass?: string;
-    startIcon?: React.ReactNode; // L'icône avant le texte
-    variant: "ghost" | "outlined" | "filled"; // Nouveau variant pour le bouton
-    buttonColor: "white" | "darkblue" | "blue" | "pink" | "black";
-    isRounded: boolean;
+  text: string; // Le texte du bouton
+  color?: string; // Couleur du texte
+  fontWeight?: "regular" | "medium" | "semibold" | "bold"; // Poids de la police
+  isActive?: boolean;
+  isDisabled?: boolean;
+  onClick?: () => void;
+  specialClass?: string;
+  startIcon?: React.ReactNode;
+  variant: "ghost" | "outlined" | "filled";
+  buttonColor: "white" | "darkblue" | "blue" | "pink" | "black";
+  isRounded: boolean;
 }
 
 const IconButton: React.FC<IconButtonProps> = ({
-                                                   text,
-                                                   fontWeight = "Normal", // Valeur par défaut pour fontWeight
-                                                   isActive = false,
-                                                   isDisabled = false,
-                                                   onClick,
-                                                   specialClass = "",
-                                                   startIcon, // L'icône passée en prop
-                                                   variant = "filled", // Valeur par défaut pour variant
-                                                   buttonColor = "blue",
-                                                   isRounded = true,// Valeur par défaut pour buttonColor
-                                               }) => {
+  text,
+  fontWeight = "Normal",
+  isActive = false,
+  isDisabled = false,
+  onClick,
+  specialClass = "",
+  startIcon,
+  variant = "filled",
+  buttonColor = "blue",
+  isRounded = true,
+}) => {
+  const buttonClass = clsx(
+    specialClass,
+    styles.iconButtonContainer,
+    styles[variant],
+    styles[buttonColor],
+    styles[fontWeight],
+    {
+      [styles.active]: isActive,
+      [styles.disabled]: isDisabled,
+      [styles.rounded]: isRounded,
+    }
+  );
 
-    // Classes dynamiques en fonction des props
-    const buttonClass = `${specialClass} ${IconButtonStyle.iconButtonContainer} 
-                          ${IconButtonStyle[variant]} 
-                          ${IconButtonStyle[buttonColor]} 
-                          ${isActive ? IconButtonStyle.active : ""} 
-                          ${isDisabled ? IconButtonStyle.disabled : ""} 
-                          ${IconButtonStyle[fontWeight]}
-                          ${isRounded ? IconButtonStyle.rounded : ""}`;
-
-    return (
-        <button
-            className={buttonClass}
-            disabled={isDisabled}
-            onClick={isDisabled ? undefined : onClick}
-            // startIcon={startIcon}
-            // sx={{color}} // Cette couleur pourrait aussi être gérée dans SCSS si tu veux
-        >
-            {startIcon && <span className={IconButtonStyle.iconBtn}>{startIcon}</span>}
-            {text}
-        </button>
-    );
+  return (
+    <button
+      className={buttonClass}
+      disabled={isDisabled}
+      onClick={isDisabled ? undefined : onClick}
+    >
+      {startIcon && <span className={styles.iconBtn}>{startIcon}</span>}
+      {text}
+    </button>
+  );
 };
 export default IconButton;
