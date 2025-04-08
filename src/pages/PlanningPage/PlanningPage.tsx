4c80b8447ec692c5f978379c17e52eb43a443b21
Elise-44 <bouardelise.perso@gmail.com>
2025-04-08 22:07:22 +0200

    reindente

--- src/pages/PlanningPage/PlanningPage.tsx
+++ src/pages/PlanningPage/PlanningPage.tsx
@@ -1,24 +1,25 @@
 import React from "react";
 import IconButton from "../../components/layout/IconButton/IconButton"; // Importer ton composant personnalisé
-import CalendarMonthOutlinedIcon from '@mui/icons-material/CalendarMonthOutlined';
-import styles from './PlanningPage.module.scss'
+import CalendarMonthOutlinedIcon from "@mui/icons-material/CalendarMonthOutlined";
+import styles from "./PlanningPage.module.scss";
 
 const PlanningPage: React.FC = () => {
-    return (
-        <div>
-            <p>Planning page</p>
-            <IconButton
-                startIcon={<CalendarMonthOutlinedIcon/>}
-                text="Planning"
-                fontWeight="regular"
-                onClick={() => console.log("Planning clicked!")}
-                specialClass={styles.specialButton}
-                isDisabled={false}
-                variant={"filled"}
-                buttonColor={"blue"}
-                isRounded={false}/>
-        </div>
-    );
+  return (
+    <div>
+      <p>Planning page</p>
+      <IconButton
+        startIcon={<CalendarMonthOutlinedIcon />}
+        text="Planning"
+        fontWeight="regular"
+        onClick={() => console.log("Planning clicked!")}
+        specialClass={styles.specialButton}
+        isDisabled={false}
+        variant={"filled"}
+        buttonColor={"blue"}
+        isRounded={false}
+      />
+    </div>
+  );
 };
 
 export default PlanningPage;
