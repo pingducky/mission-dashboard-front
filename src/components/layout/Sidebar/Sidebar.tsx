8e783a84304d749f5f55a0fe1020cff7b8af4af3
Elise-44 <bouardelise.perso@gmail.com>
2025-04-09 20:59:12 +0200

    push

--- src/components/layout/Sidebar/Sidebar.tsx
+++ src/components/layout/Sidebar/Sidebar.tsx
@@ -1,91 +1,133 @@
-import React, {useState} from 'react';
-import {Drawer, List, ListItem, ListItemText, IconButton, Divider, Box} from '@mui/material';
-import MenuIcon from '@mui/icons-material/Menu';
-import {Link} from 'react-router-dom';
-import SidebarStyle from './Sidebar.module.scss'
-import LogoCClean53 from '../../../assets/images/LogoCClean53.png'
-import CalendarMonthOutlinedIcon from '@mui/icons-material/CalendarMonthOutlined';
-import GroupsOutlinedIcon from '@mui/icons-material/GroupsOutlined';
-import ListAltOutlinedIcon from '@mui/icons-material/ListAltOutlined';
-import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
-import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
-import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined';
+import React, { useState } from "react";
+import clsx from "clsx";
+import {
+  Drawer,
+  //   List,
+  //   ListItem,
+  //   ListItemText,
+  IconButton,
+  //   Divider,
+  //   Box,
+  Button,
+} from "@mui/material";
+// import MenuIcon from "@mui/icons-material/Menu";
+// import { Link } from "react-router-dom";
+import LogoCClean53 from "../../../assets/images/LogoCClean53.png";
+import CalendarMonthOutlinedIcon from "@mui/icons-material/CalendarMonthOutlined";
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+// import GroupsOutlinedIcon from '@mui/icons-material/GroupsOutlined';
+// import ListAltOutlinedIcon from '@mui/icons-material/ListAltOutlined';
+// import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
+// import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+// import LogoutOutlinedIcon from "@mui/icons-material/LogoutOutlined";
+
+import styles from "./Sidebar.module.scss";
 
 const Sidebar: React.FC = () => {
-    const [open, setOpen] = useState(true);
+  const [open, setOpen] = useState(true);
 
-    const toggleDrawer = () => {
-        setOpen(!open);
-    };
+  const toggleDrawer = () => {
+    setOpen(!open);
+  };
 
-    return (
-        <div>
-            {/* IconButton to toggle the drawer */}
-            <IconButton color="primary" onClick={toggleDrawer}>
-                <MenuIcon/>
-            </IconButton>
-
-            {/* Drawer component */}
-            <Drawer
-                anchor="left"
-                open={open}
-                onClose={toggleDrawer}
-            >
-                <div style={{width: 250, display: 'flex', flexDirection: 'column', height: '100%'}} role="presentation"
-                     onClick={toggleDrawer} onKeyDown={toggleDrawer}>
-                    {/* Logo at the top */}
-                    <Box sx={{padding: 2, display: 'flex', justifyContent: 'center'}}>
-                        <img src={LogoCClean53} alt="Logo" style={{}}/>
-                    </Box>
-
-                    <Box sx={{padding: 2, display: 'flex', justifyContent: 'center'}}>
-                        <div>
-
-                        </div>
-                        <div>
-                            <h2>Thomas Tronville </h2>
-                            <p>Ravie de te revoir !</p>
-                        </div>
-                    </Box>
-                    <Divider/>
-                    <List className={SidebarStyle.listMenu}>
-                        <ListItem component={Link} to="/planning">
-                            <CalendarMonthOutlinedIcon/>
-                            <ListItemText primary="Planning"/>
-                        </ListItem>
-                        <ListItem component={Link} to="/salaries">
-                            <GroupsOutlinedIcon/>
-                            <ListItemText primary="Salariés"/>
-                        </ListItem>
-                        <ListItem component={Link} to="/liste-missions">
-                            <ListAltOutlinedIcon/>
-                            <ListItemText primary="Liste missions"/>
-                        </ListItem>
-                    </List>
-
-
-                    {/* Box to push the next list items to the bottom */}
-                    <Box sx={{marginTop: 'auto'}}>
-                        <Divider/>
-                        <List>
-                            <ListItem component={Link} to="/settings">
-                                <NotificationsNoneOutlinedIcon/>
-                                <ListItemText primary="Notifications"/>
-                            </ListItem>
-                            <ListItem component={Link} to="/settings">
-                                <PersonOutlineOutlinedIcon/>
-                                <ListItemText primary="Compte"/>
-                            </ListItem>
-                            <ListItem component={Link} to="/logout">
-                                <LogoutOutlinedIcon/>
-                                <ListItemText primary="Se déconnecter"/>
-                            </ListItem>
-                        </List>
-                    </Box>
-                </div>
-            </Drawer>
+  return (
+    <>
+      {/* Drawer component */}
+      <Drawer
+        anchor="left"
+        open={open}
+        onClose={toggleDrawer}
+        variant="permanent"
+        className={clsx(
+          styles.drawer,
+          open ? styles.drawerOpen : styles.drawerClosed
+        )}
+        classes={{
+          paper: clsx(open ? styles.drawerOpen : styles.drawerClosed),
+        }}
+      >
+        {/* Bouton centré verticalement à droite */}
+        <div className={styles.toggleButton}>
+          <IconButton onClick={toggleDrawer} className={styles.toggleButton}>
+            {open ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+          </IconButton>
         </div>
-    );
+
+        <div
+          role="presentation"
+          onClick={toggleDrawer}
+          onKeyDown={toggleDrawer}
+          className={styles.sidebarContainer}
+        >
+          {/* Logo at the top */}
+          {/* <Box sx={{ padding: 2, display: "flex", justifyContent: "center" }}> */}
+          {open && (
+            <div className={styles.logoSidebar}>
+              <img src={LogoCClean53} alt="Logo" />
+            </div>
+          )}
+
+          {/* </Box> */}
+
+          {/* <Box sx={{ padding: 2, display: "flex", justifyContent: "center" }}> */}
+          {open && (
+            <div className={styles.profilSidebar}>
+              <h2>Thomas Tronville </h2>
+              <p>Ravie de te revoir !</p>
+            </div>
+          )}
+
+          <hr />
+
+          {/* <List className={styles.listMenu}> */}
+          <div className={styles.iconButtonListParent}>
+            <div className={styles.iconButtonList}>
+              <Button
+                variant="outlined"
+                startIcon={<CalendarMonthOutlinedIcon />}
+              >
+                Delete
+              </Button>
+              <Button
+                variant="outlined"
+                startIcon={<CalendarMonthOutlinedIcon />}
+              >
+                Delete
+              </Button>
+              <Button
+                variant="outlined"
+                startIcon={<CalendarMonthOutlinedIcon />}
+              >
+                Delete
+              </Button>
+            </div>
+            {/* <hr/> */}
+            <div className={styles.iconButtonList}>
+              <Button
+                variant="outlined"
+                startIcon={<CalendarMonthOutlinedIcon />}
+              >
+                Delete
+              </Button>
+              <Button
+                variant="outlined"
+                startIcon={<CalendarMonthOutlinedIcon />}
+              >
+                Delete
+              </Button>
+              <Button
+                variant="outlined"
+                startIcon={<CalendarMonthOutlinedIcon />}
+              >
+                Delete
+              </Button>
+            </div>
+          </div>
+        </div>
+      </Drawer>
+    </>
+  );
 };
 
 export default Sidebar;
