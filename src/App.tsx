ec8a23d65a50a808e740a1cc3bd8153ac396428c
Elise-44 <bouardelise.perso@gmail.com>
2025-04-11 00:18:18 +0200

    modif

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import PlanningPage from './pages/PlanningPage/PlanningPage.tsx';
 import NotFoundPage from './pages/NotFoundPage/NotFoundPage.tsx';
+import EmployeesPage from './pages/EmployeesPage/EmployeesPage.tsx';
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/employees" element={<EmployeesPage />} />
         <Route path="/" element={<PlanningPage />} />
         <Route path="*" element={<NotFoundPage/>}/>
       </Routes>
