ed0c34e9b199527a044551816f32dfae9d10767f
Elise-44 <bouardelise.perso@gmail.com>
2025-04-10 19:19:40 +0200

    modif

--- src/components/layout/Sidebar/Sidebar.tsx
+++ src/components/layout/Sidebar/Sidebar.tsx
@@ -2,20 +2,15 @@ import React, { useState } from "react";
 import clsx from "clsx";
 import {
   Drawer,
-  //   List,
-  //   ListItem,
-  //   ListItemText,
   IconButton,
-  //   Divider,
-  //   Box,
   Button,
 } from "@mui/material";
 // import MenuIcon from "@mui/icons-material/Menu";
 // import { Link } from "react-router-dom";
 import LogoCClean53 from "../../../assets/images/LogoCClean53.png";
-import CalendarMonthOutlinedIcon from "@mui/icons-material/CalendarMonthOutlined";
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+// import CalendarMonthOutlinedIcon from "@mui/icons-material/CalendarMonthOutlined";
+// import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+// import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 // import GroupsOutlinedIcon from '@mui/icons-material/GroupsOutlined';
 // import ListAltOutlinedIcon from '@mui/icons-material/ListAltOutlined';
 // import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
