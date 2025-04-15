c29a8eb7668bb9aedefc1dcaa22d4be7960cadf4
Hugo <ducky4tools@gmail.com>
2025-04-15 21:56:47 +0200

    Fix self revue

--- src/hooks/useUserData.ts
+++ src/hooks/useUserData.ts
@@ -1,34 +1,46 @@
 import { useQuery } from '@tanstack/react-query';
 import { getUserDataFromToken } from '../utils/auth';
 
-// export const getUserById = async (id: string, token: string) => {
-//     const API_URL = import.meta.env.VITE_API_URL;
-
-//     return await fetch(`${API_URL}/employee/${id}`, {
-//         method: 'GET',
-//         headers: {
-//             'Content-Type': 'application/json',
-//             'Authorization': `Bearer ${token}`,
-//         },
-//     })
-//     .then(response => {
-//         return response.json();
-//     })
-//     .catch(error => {
-//         throw error;
-//     });
-// };
-
 export type User = {
+  /** 
+   * Id de l'utilisateur, utilisé pour l'identifier de manière unique.
+   **/ 
   id: number;
+  /** 
+   * Prénom de l'utilisateur. 
+   */ 
   firstName: string;
+  /** 
+   * Nom de famille de l'utilisateur. 
+   */ 
   lastName: string;
+  /** 
+   * Mot de passe de l'utilisateur. 
+   */ 
   password: string;
+  /** 
+   * Numéro de téléphone de l'utilisateur. 
+   */ 
   phoneNumber: string;
+  /** 
+   * Adresse email de l'utilisateur. 
+   */ 
   email: string;
+  /** 
+   * Adresse physique de l'utilisateur. 
+   */ 
   address: string | null;
+  /** 
+   * Indique si l'utilisateur souhaite recevoir des notifications par email. 
+   */ 
   notificationMail: boolean;
+  /** 
+   * Indique si l'utilisateur souhaite recevoir des notifications par SMS. 
+   */ 
   notificationSms: boolean;
+  /** 
+   * Indique si le compte de l'utilisateur est activé. 
+   */ 
   isEnabled: boolean;
 };
 
@@ -59,6 +71,6 @@ export const useUserData = () => {
           if (!userInfo) throw new Error('Aucun token ou ID utilisateur');
           return fetchUserById(userInfo.id, userInfo.token);
       },
-      enabled: !!userInfo, // empêche le fetch si pas d'infos
+      enabled: !!userInfo,
   });
 };
