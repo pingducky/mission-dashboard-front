f02f7b0b7b3ad071a26b5ec32c02ea6abd88d46d
Baptiste DURET <baptiste.duret@altays.com>
2025-04-30 14:29:20 +0200

    SCRUM-96

--- src/components/loading/Loading.tsx
+++ src/components/loading/Loading.tsx
@@ -2,6 +2,5 @@ import React from "react";
 import styles from "./Loading.module.scss"
 
 export const Loading: React.FC = () => (
-    <span className={styles.loader}>
-    </span>
+    <span className={styles.loader}/>
 )
