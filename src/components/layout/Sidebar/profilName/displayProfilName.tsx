25d997d38aae5edf82d9fa35b607f67f439e42f0
Elise-44 <bouardelise.perso@gmail.com>
2025-05-10 13:58:45 +0200

    ajout logo sur display name + organisation revue pour sass + ajout style + ajout isAdmin dans  displayName

--- src/components/layout/Sidebar/profilName/displayProfilName.tsx
+++ src/components/layout/Sidebar/profilName/displayProfilName.tsx
@@ -1,21 +1,45 @@
 import React from "react";
+import StarBorderPurple500OutlinedIcon from "@mui/icons-material/StarBorderPurple500Outlined";
 import styles from "./displayProfileName.module.scss";
 
 interface DisplayProfilNameProps {
-    firstname: string;
-    name: string;
-  }
-  
-const DisplayProfilName: React.FC<DisplayProfilNameProps> = ({ firstname, name }) => (
+  /**
+   * Prénom
+   */
+  firstname: string;
+  /**
+   * Nom
+   */
+  name: string;
+  /**
+   * Vérifier si admin
+   */
+  isAdmin?: boolean;
+}
+
+const DisplayProfilName: React.FC<DisplayProfilNameProps> = ({
+  firstname,
+  name,
+  isAdmin,
+}) => {
+  console.log("isAdmin : ", isAdmin);
+
+  return (
     <div className={styles.profileContainer}>
-        <div className={styles.avatar}>
-            <span className={styles.initial}>{firstname.charAt(0)}</span>
-        </div>
-        <div className={styles.info}>
-            <h2 className={styles.name}>{firstname} {name}</h2>
-            <p className={styles.subtitle}>Ravie de te revoir ici !</p>
-        </div>
+      <div className={styles.avatar}>
+        {isAdmin && (
+          <StarBorderPurple500OutlinedIcon className={styles.adminIcon} />
+        )}
+        <span className={styles.initial}>{firstname.charAt(0)}</span>
+      </div>
+      <div className={styles.info}>
+        <h2 className={styles.name}>
+          {firstname} {name}
+        </h2>
+        <p className={styles.subtitle}>Ravie de te revoir ici !</p>
+      </div>
     </div>
-);
+  );
+};
 
 export default DisplayProfilName;
