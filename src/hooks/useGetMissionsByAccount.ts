e9617dd8f6467c1fe63bd5013b144c5efaa3393b
Elise-44 <bouardelise.perso@gmail.com>
2025-05-17 12:12:12 +0200

    correction route utilisé pour filtrer missions + organisation filters

--- src/hooks/useGetMissionsByAccount.ts
+++ src/hooks/useGetMissionsByAccount.ts
@@ -92,14 +92,48 @@ type Params = {
     limit?: number;
 };
 
-const getMissionsByAccount = async ({ accountId, from, to, filterByType, limit }: Params): Promise<Mission[]> => {
+type CategorizedMissions = {
+  past: Mission[];
+  current: Mission[];
+  future: Mission[];
+};
+
+// const getMissionsByAccount = async ({ accountId, from, to, filterByType, limit }: Params): Promise<Mission[]> => {
+//     const params = new URLSearchParams();
+
+//     if (from) params.append("from", from);
+//     if (to) params.append("to", to);
+//     if (filterByType !== undefined) params.append("filterByType", filterByType.toString());
+//     if (limit !== undefined) params.append("limit", limit.toString());
+//     const url = `${API_URL}/mission/missionCategorized/${accountId}?${params.toString()}`;
+
+//     const response = await fetch(url, {
+//         method: "GET",
+//         headers: {
+//             "Content-Type": "application/json",
+//             "Authorization": `Bearer ${sessionStorage.getItem("token")}`,
+//         },
+//     });
+
+//     if (!response.ok) {
+//         throw new Error("Erreur lors de la récupération des missions");
+//     }
+
+//     const data = await response.json();
+//     // return data.missions;
+//     return {
+//         past: data.past || [],
+//         current: data.current || [],
+//         future: data.future || []
+//     };
+// };
+const getMissionsByAccount = async ({ accountId, filters, limit }: Params): Promise<CategorizedMissions> => {
     const params = new URLSearchParams();
 
-    if (from) params.append("from", from);
-    if (to) params.append("to", to);
-    if (filterByType !== undefined) params.append("filterByType", filterByType.toString());
+    if (filters) params.append("filters", filters.join(","));
     if (limit !== undefined) params.append("limit", limit.toString());
-    const url = `${API_URL}/mission/listMissions/${accountId}?${params.toString()}`;
+
+    const url = `${API_URL}/mission/missionCategorized/${accountId}?${params.toString()}`;
 
     const response = await fetch(url, {
         method: "GET",
@@ -114,11 +148,15 @@ const getMissionsByAccount = async ({ accountId, from, to, filterByType, limit }
     }
 
     const data = await response.json();
-    return data.missions;
+    return {
+        past: data.past ?? [],
+        current: data.current ?? [],
+        future: data.future ?? []
+    };
 };
 
 export const useGetMissionsByAccount = (params: Params, enabled = true) => {
-    return useQuery({
+    return useQuery<CategorizedMissions>({
         queryKey: ["missions", params],
         queryFn: () => getMissionsByAccount(params),
         enabled,
